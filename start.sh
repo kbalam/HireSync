2c4f0c91e5b358600e6bdd6d8834ff7e6412fc83
Your Name <balabm2001@outlook.com>
2025-03-08 18:26:17 +0530

    updates made on the start.sh and render.yaml

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Installing dependencies..."
-pip install -r backend/requirements.txt  # Point to requirements.txt in backend folder
-echo "Dependencies installed successfully."
 echo "Starting Flask app..."
-cd /opt/render/project/src/
-python backend/app.py
+cd backend
+python app.py
