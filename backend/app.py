3b743ca299dea467ba8493cb495518af55f3074f
Your Name <balabm2001@outlook.com>
2025-03-09 12:12:28 +0530

    modified the cors for the frontend link

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("APP_SECRET_KEY")
-CORS(app, origins=["https://hiresync.netlify.app/"])  
+CORS(app, origins=["https://hiresync.netlify.app"])  
 
 
 @app.route("/upload", methods=["POST"])
